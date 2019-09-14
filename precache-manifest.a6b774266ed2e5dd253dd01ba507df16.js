7d6f2a84845ce9cea4db572951191bb27b4c976c
KseniiaMazan <ksusamazan@gmail.com>
2019-09-14 18:59:03 +0300

    Updates

---
+++ precache-manifest.a6b774266ed2e5dd253dd01ba507df16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d99639886256e1bd0f561436d3413a",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "563700827c95306eeced",
+    "url": "/react_list-of-goods/static/css/main.69669a43.chunk.css"
+  },
+  {
+    "revision": "309df93e1a0b7285bf6a",
+    "url": "/react_list-of-goods/static/js/2.c9130f23.chunk.js"
+  },
+  {
+    "revision": "563700827c95306eeced",
+    "url": "/react_list-of-goods/static/js/main.11119e3e.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
